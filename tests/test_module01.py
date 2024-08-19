095a2240ee24b3ed8360a06286a27702978d6a80
ricardojaj <rickanjos85@gmail.com>
2024-08-19 20:41:28 -0300

    Subindo projeto

---
+++ tests/test_module01.py
@@ -0,0 +1,15 @@
+def test_a1():
+    assert 5 + 5 == 10;
+    assert 5 - 5 == 0;
+    assert 5 * 5 == 25;
+    assert 5 / 5 == 1;
+
+def test_a2():
+    assert 9/5 == 1.5, "failed test intentionally";
+
+def test_a3():
+    assert 9//5 == 1;
+
+def test_a4():
+    assert 19.5 + 19.5 == 39;
+
