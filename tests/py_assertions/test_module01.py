b4f6c174a55fa58e3e4274c0a671b97a4d9bef7f
ricardojaj <rickanjos85@gmail.com>
2024-08-27 21:05:21 -0300

    novo update - classe TestMyStuff

--- tests/py_assertions/test_module01.py
+++ tests/py_assertions/test_module01.py
@@ -1,4 +1,11 @@
 def test_a1():
     assert 4 < 5
 
+def test_a2():
+    assert 'teste' == 'teste'
+
+def test_a3():
+    assert 1 in divmod(9, 5)
+    assert 'py' in 'this is pytest'
+    assert [1, 2] == [1, 2]
 
